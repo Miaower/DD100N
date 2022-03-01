4e007965b4d60b3593d636215f0aca32d16d3b5b
Linnéa Sandblom <linnea.sandblom@live.se>
2022-03-01 23:01:58 +0100

    Commit lab 6

--- linnea_sandblom_5.py
+++ linnea_sandblom_5.py
@@ -1,46 +1,106 @@
-# Inlämningsuppgift 5, DD100N, Linnéa Sandblom, 2022-xx-xx
-# Programmet håller koll på och visar poäng för en pilkastningstävling.
-# Det läser in och sparar poängställningen till en textfil.
+# Inlämningsuppgift 5, DD100N, Linnéa Sandblom, 2022-03-01
+# The program keeps count of the score of a dart competition.
+# It reads the score from, and saves the score to a text file.
 import ast
 
 
 def read_file(in_file="results2022.txt"):
+    """
+    Reads from the text file(in_file) into the list competitors
+    :param in_file: name of text-file containing competitors and their scores
+    :return: competitors: list containing competitors and their scores
+    """
     competitors = []
     with open(in_file, "r+", encoding="utf-8") as file:
         for line in file:
             competitors.append(ast.literal_eval(line.strip()))
     return competitors
 
+
 def view_results(competitors):
-    for element in competitors:
-        print(element)
+    """
+    Sorts and views results in order of highest to lowest scores
+    :param competitors: list containing competitors and their scores
+    """
+    results = sorted(competitors, key=lambda person: person[1], reverse=True)
+    for element in results:
+        print(f" {element[0]}: {element[1]} points")
 
 
 def enter_results(competitors):
-    input()
+    """
+    Enters new player and score to the list
+    :param competitors: list containing competitors and their scores
+    :return: competitors: list containing competitors and their scores, with added player and score
+    """
+    new_name = input("Name of competitor: ")
+    while not check_name(competitors, new_name):
+        new_name = input("This name is already taken. Enter new name of competitor: ")
+
+    new_result = input(f"How many points did {new_name} get? ")
+    while not check_result(new_result):
+        new_result = input(f"Invalid input. Enter a result between 0-50. How many points did {new_name} get? ")
+
+    competitors.append([new_name, int(new_result)])
     return competitors
 
 
-def save_quit(competitors=None):
-    if competitors is None:
-        competitors = [["Kalle", 3], ["Lisa", 50]]
-    with open("results2022.txt", "w") as file:
+def check_name(competitors, new_name):
+    """
+    Checks if name is taken
+    :param competitors: list containing competitors and their scores
+    :param new_name: name to check
+    :return: False if name is already taken, else True
+    """
+    for element in competitors:
+        if element[0] == new_name:
+            return False
+    return True
+
+
+def check_result(new_result):
+    """
+    Checks if score is int and if it is in the requested range 0-50.
+    :param new_result: score to be checked
+    :return: False if new_result is not an int or not in range 0-50, else True
+    """
+    try:
+        new_result = int(new_result)
+    except ValueError:
+        return False
+    if new_result not in range(0, 51):
+        return False
+    return True
+
+
+def save_quit(competitors, file="results2022.txt"):
+    """
+    Saves list of competitors to text file.
+    :param competitors: list containing competitors and their scores
+    :param file: filename to save to
+    """
+    with open(file, "w") as file:
         for element in competitors:
             file.write(str(element) + "\n")
     print("Saved, welcome back!")
 
 
 def main():
+    """
+    Calls function to read text file with competition data. Let's user choose between the actions
+    "View results", "Enter new result" and "Save and quit"
+    """
     competitors = read_file()
-    print("1. View results \n2. Enter new result \n3. Save and quit")
     while True:
+        print("\n1. View results \n2. Enter new result \n3. Save and quit")
         choice = input("Choose an alternative 1-3: ")
+        print()
         if choice == "1":
             view_results(competitors)
         elif choice == "2":
             enter_results(competitors)
         elif choice == "3":
-            save_quit()
+            save_quit(competitors)
             break
 
 
--- linnea_sandblom_6.py
+++ linnea_sandblom_6.py
@@ -1,3 +1,46 @@
 # Inlämningsuppgift 6, DD100N, Linnéa Sandblom, 2022-xx-xx
-# [Beskrivning av programmet]
+# Controls a pet care robot. Finds pets, feeds and pets them.
 
+def list_pets():
+    return True
+
+
+def find_pet():
+    pet = input("Which pet do you want me to find? ")
+    pet_care(pet)
+
+
+def pet_pet():
+    return True
+
+
+def feed_pet():
+    return True
+
+
+def pet_care(pet):
+    print(f"Found {pet}. What should I do now?\n1. Pet {pet} \n2. Feed {pet} \n3. Main menu")
+    choice = input("Choose an alternative 1-3: ")
+    if choice == "1":
+        pet_pet()
+    elif choice == "2":
+        feed_pet()
+    elif choice == "3":
+        break
+
+def main():
+    while True:
+        print("\n1. List pets and their statuses \n2. Find pet \n3. Quit")
+        choice = input("Choose an alternative 1-3: ")
+        print()
+        if choice == "1":
+            list_pets()
+        elif choice == "2":
+            find_pet()
+        elif choice == "3":
+            print("Welcome back!")
+            break
+
+
+if __name__ == "__main__":
+    main()
